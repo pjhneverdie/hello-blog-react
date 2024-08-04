b8f3d68911a83379a3a188c61376f097b4bbc2ba
박진혁 <park@bagjinhyeog-ui-MacBookAir.local>
2024-08-04 18:06:10 +0900

    add: 필수 패키지 추가 프로젝트 생성 후 포크 완료 및 필수 패키지(라우터, 차크라)를 추가 했음.

---
+++ src/home/page/HomePage.jsx
@@ -0,0 +1,11 @@
+import {Flex} from "@chakra-ui/react"
+
+function HomePage() {
+    return (
+        <Flex  flexDirection="column" justify="center" align="center">
+            Flex Container
+        </Flex>
+    );
+}
+
+export default HomePage;
